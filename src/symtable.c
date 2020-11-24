85d4494d32c7620f694e3d2970b5922a71e065b2
timom26 <xkamen24@stud.fit.vutbr.cz>
2020-11-24 17:58:37 +0100

    WIP formatting and something

--- src/symtable.c
+++ src/symtable.c
@@ -12,63 +12,71 @@
  * @date 2020-11-20
  */
 
-
 #include <string.h>
 #include <stdlib.h>
 #include "symtable.h"
 #include <stdio.h>
 unsigned long hash(char *str)
 {
-	unsigned long hash = 0, x = 0;
-
-	for (char c = *str; c != '\0'; c = *(++str))
-	{
-		hash = (hash << 4) + c;
-		if ((x = hash & 0xF0000000L) != 0)
-		{
-			hash ^= (x >> 24);
-		}
-		hash &= ~x;
-	}
-
-	return hash % MAX_SIZE_OF_SYM_TABLE;
+    unsigned long hash = 0;
+    unsigned long x = 0;
+    for (char letter = *str; letter != '\0'; letter = *(++str))
+    {
+        hash = (hash << 4) + letter;
+    }
+    return hash % MAX_SIZE_OF_SYM_TABLE;
 }
 
-
-void symtable_init(Symtable * symtable){
-    if (symtable == NULL){
+void symtable_init(Symtable *symtable)
+{
+    if (symtable == NULL)
+    {
         return;
     }
-    for(int i = 0;i<MAX_SIZE_OF_SYM_TABLE;i++){//NULL ALL
-        (*symtable)[i]= NULL;
+    for (int i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    { //NULL ALL
+        (*symtable)[i] = NULL;
     }
 }
 
-Symtable_item * symtable_add(Symtable * table, char * key,bool * noerror){
-    if (table == NULL || key == NULL){//initial check
+Symtable_item *symtable_add(Symtable *table, char *key, bool *noerror)
+{
+    if (table == NULL || key == NULL)
+    { //initial check
         *noerror = false;
         return NULL;
     }
     //make sure symbol is not in the table already
     unsigned long hash_index = hash(key);
-    Symtable_item * tmp = (*table)[hash_index];
-    while(tmp != NULL){
-        if(strcmp(tmp->key,key) == 0){
+    Symtable_item *tmp = (*table)[hash_index];
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
             *noerror = true;
             return NULL;
         }
         tmp = tmp->next;
     }
-    
-    //if not found, make new one
-    Symtable_item * new_item = malloc(sizeof(Symtable_item));                   //MALLOC
-    if (new_item == NULL){*noerror = false;return NULL;}
 
-    new_item->key = (char *)malloc(sizeof(char)*(strlen(key)+1));              //MALLOC
-    if (new_item->key == NULL){free(new_item);*noerror = false; return NULL;}
+    //if not found, make new one
+    Symtable_item *new_item = malloc(sizeof(Symtable_item)); //MALLOC
+    if (new_item == NULL)
+    {
+        *noerror = false;
+        return NULL;
+    }
+
+    new_item->key = (char *)malloc(sizeof(char) * (strlen(key) + 1)); //MALLOC
+    if (new_item->key == NULL)
+    {
+        free(new_item);
+        *noerror = false;
+        return NULL;
+    }
 
     //TODO FILL IN DATA
-    strcpy(new_item->key,key);
+    strcpy(new_item->key, key);
     //put in table
     tmp = (*table)[hash_index];
     (*table)[hash_index] = new_item;
@@ -76,56 +84,70 @@ Symtable_item * symtable_add(Symtable * table, char * key,bool * noerror){
     *noerror = true;
     return new_item;
 }
-Symtable_item * symtable_search(Symtable * table,char * key){
+Symtable_item *symtable_search(Symtable *table, char *key)
+{
     unsigned long hash_index = hash(key);
-    Symtable_item * tmp = (*table)[hash_index];
-    while(tmp != NULL){
-        if (strcmp(tmp->key,key) == 0){
+    Symtable_item *tmp = (*table)[hash_index];
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
             return tmp;
         }
         tmp = tmp->next;
     }
     return NULL;
 }
-void symtable_remove(Symtable * table, char * key){
+void symtable_remove(Symtable *table, char *key)
+{
     unsigned long hash_index = hash(key);
-    Symtable_item * current = (*table)[hash_index];
-    Symtable_item * previous = NULL;
+    Symtable_item *current = (*table)[hash_index];
+    Symtable_item *previous = NULL;
     bool found = false;
-    while (current != NULL){
-        if (strcmp(key,current->key) == 0){
+    while (current != NULL)
+    {
+        if (strcmp(key, current->key) == 0)
+        {
             found = true;
             break;
         }
         previous = current;
         current = current->next;
     }
-    if (found == false){return;}
-    
-    //link the other items in table
-    if (previous == NULL){//if item was first on hash
-        (*table)[hash_index] = current->next;//works even if only one in list on the hash
+    if (found == false)
+    {
+        return;
     }
-    else{//item was not first in list
+
+    //link the other items in table
+    if (previous == NULL)
+    {                                         //if item was first on hash
+        (*table)[hash_index] = current->next; //works even if only one in list on the hash
+    }
+    else
+    { //item was not first in list
         previous->next = current->next;
     }
     //free item
-    free(current->key);                                     //FREE
-    free(current);                                          //FREE
+    free(current->key); //FREE
+    free(current);      //FREE
     return;
 }
 
-void symtable_free(Symtable* table){
-    Symtable_item * current;
-    Symtable_item * next;
-    for (int i = 0; i<MAX_SIZE_OF_SYM_TABLE;i++){
+void symtable_free(Symtable *table)
+{
+    Symtable_item *current;
+    Symtable_item *next;
+    for (int i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    {
         current = (*table)[i];
-        while (current != NULL){
+        while (current != NULL)
+        {
             next = current->next;
             free(current->key);
             free(current);
             current = next;
         }
-        (*table)[i]=NULL;//is it really needed?
+        (*table)[i] = NULL; //is it really needed?
     }
 }
--- tests/symtable_valgrind.c
+++ tests/symtable_valgrind.c
@@ -2,90 +2,131 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
-
-
-void printboolean(bool boolean){
-    if (boolean){
+void printboolean(bool boolean)
+{
+    if (boolean)
+    {
         printf("boolean is true");
     }
-    else{
+    else
+    {
         printf("boolean is false");
     }
 }
 
-
-int main(){
-        Symtable table;
+int main()
+{
+    Symtable table;
     symtable_init(&table);
-        for (int i = 0;i<MAX_SIZE_OF_SYM_TABLE;i++)
+    for (int i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    {
+        if (table[i] != NULL)
         {
-            if (table[i]!=NULL)
+            printf("NOT noice\n");
+        }
+    }
+    bool noerror = false;
+
+    Symtable_item *firstitem = symtable_add(&table, "Timotej", &noerror);
+
+    if (noerror != true)
+    {
+        printf("error! failed memory allocation.\n");
+        return 0;
+    }
+    if (firstitem == NULL)
+    {
+        printf("error! shouldnt be null pointer.\n");
+    }
+    printf("%s\n", firstitem->key);
+    if (table[hash("Timotej")] == NULL)
+    {
+        printf("not saved in table.\n");
+    }
+    else if (table[hash("Timotej") != firstitem])
+    {
+        printf("incorrect save in table");
+    }
+    else
+    {
+        printf("all correct.\n");
+    }
+    for (unsigned long i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    {
+        if (table[i] != NULL)
+        {
+            if (i == hash("Timotej"))
             {
-                printf("NOT noice\n");
+                printf("found it.\n");
+            }
+            else
+            {
+                printf("incorrect write into table.\n");
             }
         }
-        bool noerror = false;
-        
-    Symtable_item * firstitem = symtable_add(&table,"Timotej", &noerror);
-        
-        if (noerror != true){printf("error! failed memory allocation.\n");return 0;}
-        if (firstitem == NULL){printf("error! shouldnt be null pointer.\n");}
-        printf("%s\n",firstitem->key);
-        if (table[hash("Timotej")] == NULL)             {printf("not saved in table.\n");}
-        else if (table[hash("Timotej") != firstitem])   {printf("incorrect save in table");}
-        else                                            {printf("all correct.\n");}
-        for (unsigned long i = 0;i<MAX_SIZE_OF_SYM_TABLE;i++){
-            if (table[i]!= NULL){
-                if (i == hash("Timotej")){
-                    printf("found it.\n");
-                }
-                else{
-                    printf("incorrect write into table.\n");
-                }
-            }
-        }
+    }
     symtable_remove(&table, "Timotej");
-        for (int i = 0;i<MAX_SIZE_OF_SYM_TABLE;i++)
+    for (int i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    {
+        if (table[i] != NULL)
         {
-            if (table[i]!=NULL)
+            printf("NOT noice\n");
+        }
+    }
+
+    Symtable_item *seconditem = symtable_add(&table, "Matej", &noerror);
+    if (noerror != true)
+    {
+        printf("error! failed memory allocation.\n");
+        return 0;
+    }
+    if (firstitem == NULL)
+    {
+        printf("error! shouldnt be null pointer.\n");
+    }
+    if (table[hash("Matej")] == NULL)
+    {
+        printf("not saved in table.\n");
+    }
+    else if (table[hash("Matej") != firstitem])
+    {
+        printf("incorrect save in table");
+    }
+    else
+    {
+        printf("all correct.\n");
+    }
+    for (unsigned long i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    {
+        if (table[i] != NULL)
+        {
+            if (i == hash("Matej"))
             {
-                printf("NOT noice\n");
+                printf("found it.\n");
+            }
+            else
+            {
+                printf("incorrect write into table.\n");
             }
         }
-    //to this point, checked with valgrind, all cleared
-Symtable_item * seconditem = symtable_add(&table,"Matej", &noerror);
-    if (noerror != true){printf("error! failed memory allocation.\n");return 0;}
-    if (firstitem == NULL){printf("error! shouldnt be null pointer.\n");}
-    if (table[hash("Matej")] == NULL)                   {printf("not saved in table.\n");}
-        else if (table[hash("Matej") != firstitem])     {printf("incorrect save in table");}
-        else                                            {printf("all correct.\n");}
-    for (unsigned long i = 0;i<MAX_SIZE_OF_SYM_TABLE;i++){
-            if (table[i]!= NULL){
-                if (i == hash("Matej")){
-                    printf("found it.\n");
-                }
-                else{
-                    printf("incorrect write into table.\n");
-                }
-            }
-        }
+    }
     symtable_free(&table);
-    for (int i = 0;i<MAX_SIZE_OF_SYM_TABLE;i++)
+    for (int i = 0; i < MAX_SIZE_OF_SYM_TABLE; i++)
+    {
+        if (table[i] != NULL)
         {
-            if (table[i]!=NULL)
-            {
-                printf("NOT noice\n");
-            }
+            printf("NOT noice\n");
         }
-Symtable_item * thirditem = symtable_add(&table,"Lukas", &noerror);
-Symtable_item * founditem = symtable_search(&table,"Lukas");
-if (founditem != thirditem){
-    printf("they do not equal");
-}
-printf("%s\n",founditem->key);
-symtable_remove(&table,"Lukas");
+    }
+    Symtable_item *thirditem = symtable_add(&table, "Lukas", &noerror);
+    Symtable_item *founditem = symtable_search(&table, "Lukas");
+    if (founditem != thirditem)
+    {
+        printf("they do not equal");
+    }
+    printf("%s\n", founditem->key);
+    symtable_remove(&table, "Lukas");
+    //to this point, checked with valgrind, all cleared
     printf("#######################    DONE    ##############################################################################################################################################################################\n");
     return 0;
 }
