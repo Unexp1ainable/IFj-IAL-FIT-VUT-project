d776faa79258a4007c6729ab46b0b75c01f6f03a
Wooo99 <michal.reznik2000@gmail.com>
2020-11-28 23:13:44 +0100

    start of expresion(stack, help functions)

---
+++ src/expression.c
@@ -0,0 +1,16 @@
+/**
+ * @file expression.c
+ * 
+ * @author Timotej Kamenský, xkamen24
+ * @author Michal Řezník,  xrezni28
+ * @author Jiří Hofírek, xhofir06
+ * @author Samuel Repka, xrepka07
+ * 
+ * @brief  Processing of expressions by precedent analysis
+ */
+#include"expression.h"
+
+
+void InitStack(Stack *MyStack){
+
+}
