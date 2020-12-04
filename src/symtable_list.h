d3fd3e43fd15310eb039032bbae8af49ee13424f
Samuel <xrepka07@stud.fit.vutbr.cz>
2020-12-04 19:04:52 +0100

    Relocated again

--- src/symtable_list.h
+++ src/symtable_list.h
@@ -12,6 +12,18 @@
 #include "symtable.h"
 
 
+// ###################### structs ####################
+
+/*************TYPE OF TERMS*************/
+typedef enum{
+    T_INT,              //integer
+    T_FLOAT,            //floater
+    T_STRING,           //string
+    T_BOOL,             //true/false
+    T_UNKNOWN           //variable use
+} TermType;
+// ################### end of structs #################
+
 /**
  * @brief Item of linked list of symtables
  * 
