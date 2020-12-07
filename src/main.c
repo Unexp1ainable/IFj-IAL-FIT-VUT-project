9b689239738bd63ab05b8ceabdc60f4f039f463d
timom26 <xkamen24@stud.fit.vutbr.cz>
2020-12-07 21:14:45 +0100

    corrected main.c print_token function

--- src/main.c
+++ src/main.c
@@ -4,17 +4,52 @@
 void print_token(TOKEN t, int code, FILE *fptr)
 {
     const char *t_type_str[] = {
-        "TOKEN_TYPE_ADD", "TOKEN_TYPE_SUBTRACT", "TOKEN_TYPE_MULTIPLY", "TOKEN_TYPE_DIVIDE", "TOKEN_TYPE_EQUAL",
-        "TOKEN_TYPE_NOT_EQUAL", "TOKEN_TYPE_LOWER_THAN", "TOKEN_TYPE_GREATER_THAN", "TOKEN_TYPE_LOWER_EQUAL",
-        "TOKEN_TYPE_GREATER_EQUAL", "TOKEN_TYPE_SUB1", "TOKEN_TYPE_ADD1", "TOKEN_TYPE_OPENING_CLASSIC_BRACKET",
-        "TOKEN_TYPE_CLOSING_CLASSIC_BRACKET", "TOKEN_TYPE_OPENING_CURVY_BRACKET", "TOKEN_TYPE_CLOSING_CURVY_BRACKET",
-        "TOKEN_TYPE_COMMA", "TOKEN_TYPE_SEMICOLON", "TOKEN_TYPE_UNDERSCORE", "TOKEN_TYPE_EOF", "TOKEN_TYPE_EOL",
-        "TOKEN_TYPE_IDENTIFIER", "TOKEN_TYPE_STRING", "TOKEN_TYPE_INTEGER", "TOKEN_TYPE_FLOAT64", "TOKEN_TYPE_RESERVED_KEYWORD",
-        "TOKEN_TYPE_DEFINE_AND_ASSIGN", "TOKEN_TYPE_ASSIGN", "TOKEN_TYPE_MAIN"};
+        "TOKEN_TYPE_ADD", 
+        "TOKEN_TYPE_SUBTRACT", 
+        "TOKEN_TYPE_MULTIPLY", 
+        "TOKEN_TYPE_DIVIDE",
+
+        "TOKEN_TYPE_EQUAL",
+        "TOKEN_TYPE_NOT_EQUAL", 
+        "TOKEN_TYPE_LOWER_THAN", 
+        "TOKEN_TYPE_GREATER_THAN", 
+        "TOKEN_TYPE_LOWER_EQUAL",
+        "TOKEN_TYPE_GREATER_EQUAL", 
+        "TOKEN_TYPE_SUB1", 
+        "TOKEN_TYPE_ADD1", 
+
+        "TOKEN_TYPE_OPENING_CLASSIC_BRACKET",
+        "TOKEN_TYPE_CLOSING_CLASSIC_BRACKET", 
+        "TOKEN_TYPE_OPENING_CURVY_BRACKET", 
+        "TOKEN_TYPE_CLOSING_CURVY_BRACKET",
+        "TOKEN_TYPE_COMMA", 
+        "TOKEN_TYPE_SEMICOLON", 
+        "TOKEN_TYPE_UNDERSCORE", 
+        "TOKEN_TYPE_EOF", 
+        "TOKEN_TYPE_EOL",
+        "TOKEN_TYPE_IDENTIFIER", 
+
+        "TOKEN_TYPE_RESERVED_KEYWORD",
+        "TOKEN_TYPE_STRING", 
+        "TOKEN_TYPE_INTEGER", 
+        "TOKEN_TYPE_FLOAT64", 
+        
+        "TOKEN_TYPE_DEFINE", 
+        "TOKEN_TYPE_ASSIGN", 
+        "TOKEN_TYPE_MAIN"};
 
     const char *keyword_str[] = {
-        "KEYWORD_ELSE", "KEYWORD_FLOAT64", "KEYWORD_FOR", "KEYWORD_FUNC", "KEYWORD_IF", "KEYWORD_INT", "KEYWORD_PACKAGE",
-        "KEYWORD_RETURN", "KEYWORD_STRING", "KEYWORD_NONE"};
+        "KEYWORD_ELSE", 
+        "KEYWORD_FOR", 
+        "KEYWORD_FUNC", 
+        "KEYWORD_IF", 
+        "KEYWORD_PACKAGE",
+        "KEYWORD_RETURN", 
+        "KEYWORD_NONE"
+        "KEYWORD_FLOAT64", 
+        "KEYWORD_INT", 
+        "KEYWORD_STRING", 
+        };
 
     fprintf(fptr,"------New token------\n");
     fprintf(fptr,"Type: %s\n", t_type_str[t.tokentype]);
@@ -74,9 +109,11 @@ int main(/*int argc, char *argv[]*/)
     init_token_list(list);
     int r_code = 0;
     printf("lets start:\n");
+    fprintf(fptr1,"this is file containing tokens from original scanner.\n\n");
+    fprintf(fptr2,"this is file containing tokens from saved token list.\n\n");
     while (!end && !r_code)
     {
-        printf("\n\n->");
+        //printf("\n\n->");
         r_code = get_next_token(t,list);
         if(r_code == 0)
             print_token(*t, r_code,fptr1);
@@ -87,7 +124,12 @@ int main(/*int argc, char *argv[]*/)
         print_token(*t,r_code,fptr2);
     }
     free_token_list(list);
+    //dynamic_string_free(t->string);
+    //free(t->string);
+    free(t);
     free(list);
     free(stringbuffer.string);
+    fclose(fptr1);
+    fclose(fptr2);
     return r_code;
 }
