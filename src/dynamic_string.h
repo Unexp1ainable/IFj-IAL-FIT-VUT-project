fa4461fa7177dde8924805474d3e7e9daaa0e9ac
timom26 <xkamen24@stud.fit.vutbr.cz>
2020-12-08 16:44:50 +0100

    added comments to the header files

--- src/dynamic_string.h
+++ src/dynamic_string.h
@@ -14,15 +14,65 @@ typedef struct{
     unsigned int allocated_size;
     int error;
 }Dynamic_string;
-
+/**
+ * @brief initialise the dynamic string and allocate the default length of the dynamic string
+ * @return true if successful, false if memory operation failed
+ * */
 bool dynamic_string_init        (Dynamic_string * dynamicstring);
+
+
+/**
+ * @brief append the dynamic string by the given string. auto-increase size of string
+ * @return true if successful, false if memory operation failed
+ * */
 bool dynamic_string_add_char    (Dynamic_string * dynamicstring, char c);
+
+
+/**
+ * @brief append the dynamic string by the given string. aut-increase size of string
+ * @return true if successful, false if memory operation failed
+ * */
 bool dynamic_string_add_string  (Dynamic_string * dynamicstring, char * word);
+
+
+/**
+ * @brief double the allocated size of the dynamic string
+ * @return true if successful, false if memory operation failed
+ * */
 bool dynamic_string_double      (Dynamic_string * dynamicstring);
+
+
+/**
+ * @brief free internals of dynamic string and set metadata
+ * */
 void dynamic_string_free        (Dynamic_string * dynamicstring);
+
+
+/**
+ * @brief set all characters of dynamic string to null and set metadata
+ * */
 void dynamic_string_delete      (Dynamic_string * dynamicstring);
+
+
+/**
+ * @brief is string on its max allocated length?
+ * @return true if full, false if not full. 
+ * */
 bool dynamic_string_full        (Dynamic_string * dynamicstring);
+
+
+/**
+ * @brief delete last letter from Dynamic_string (that is: set to null and lower size of string)
+ * */
 void dynamic_string_backspace   (Dynamic_string * dynamicstring);
+
+
+/**
+ * @brief print string inside Dynamic_string
+ * */
 void dynamic_string_print       (Dynamic_string dynamicstring);
+
+
+
 
 #endif
--- src/newscanner.h
+++ src/newscanner.h
@@ -141,69 +141,83 @@ typedef struct{
 
 /**
  * @brief fill the tokenptr by next token in the file, also save copy in the list of tokens
+ * @return OK (0), WTF(aka Lex_error)(1),memoryerror
  * */
 int get_next_token      (TOKEN * tokenptr, TokenList * tokenlist);
 
 
 /**
- * @brief fille the tokenptr by next saved token in the list of tokens
+ * @brief set tokenptr value as a pointer to the next token in tokenlist
+ * @return false if reached the end of list, true if found the token
  * */
 bool get_next_token_list(TOKEN ** tokenptr, TokenList * tokenlist);
 
 /**
  * @brief assistive function. Set the scanner status. Best left untouched
+ * @return void
  * */
 void set_fsm_state      (FSM_STATES input);
 
 /**
  * @brief create a token with float value
+ * @return void
  * */
 void make_token_float   (TOKEN * t,        char* num);
 
 
 /**
  * @brief copy the dynamic string inside a token dynamic string
+ * @return false if failed (most likely memory eror), true if succeeded
  * */
 bool dynamic_string_copy(TOKEN * token,    Dynamic_string *dynamicstring);
 
 
 /**
  * @brief save a token into a token list. Assistive function
+ * @return false if failed (most likely memory eror), true if succeeded
  * */
 bool save_next_token    (TokenList * tokenlist,TOKEN * token);
 
 
 /**
  * @brief initialise the tokenlist values. Valgrind doesnt like uninitialised values. Make it happy
+ * @return void
  * */
 void init_token_list    (TokenList * tokenlist);
 
 
 /**
  * @brief copy the data of src token to dest token
+ * @return true if succeeded, false if failed (most likely memory eror)
  * */
 bool copy_token         (TOKEN * dest, TOKEN * src);
 
 
 /**
  * @brief copy string from one token to another token
+ * @return true if succeeded, false if failed (most likely memory eror)
  * */
 bool dynamic_string_copy_string (TOKEN * dest,TOKEN * src);
 
 
 /**
  * @brief free all tokens saved in the list of tokens
+ * @return void
  * */
 void free_token_list    (TokenList * tokenlist);
 
 
 /**
  * @brief set list position to 0
+ * @return void
  * */
 void reset_list_position(TokenList *t_list);
 
 
-
+/**
+ * @brief get next token until there isnt EOF. Basically runs the lexical analysis
+ * @return any error code in lexical analysis stops the program, else OK (0)
+ * */
 int load_tokens(TokenList *t_list);
 
 // variables
